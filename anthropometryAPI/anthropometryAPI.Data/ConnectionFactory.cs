34e0de2c253808b7458d0f79a923a831e7339bc7
Diego <diegosampaiorj@gmail.com>
2020-10-25 00:17:19 -0300

    separando projetos e instalando pacotes iniciais

---
+++ anthropometryAPI/anthropometryAPI.Data/ConnectionFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace anthropometryAPI.Data
+{
+    public static class ConnectionFactory
+    {
+    }
+}
---
+++ anthropometryAPI/anthropometryAPI.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace anthropometryAPI.Service
+{
+    public class Class1
+    {
+    }
+}
